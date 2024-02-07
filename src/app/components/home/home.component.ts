0a4c4c6bca5eab35bc7037961aed5049635a513d
Vitthal Gadekar <gadekarvitthal2@gmail.com>
2024-02-07 22:49:05 +0530

    - initial commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, TemplateRef } from '@angular/core';
+
+import {NgbOffcanvas} from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+
+})
+export class HomeComponent {
+
+  private offcanvasService = inject(NgbOffcanvas);
+	closeResult = '';
+
+	open(content: TemplateRef<any>) {
+    console.log('content :>> ', content);
+		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				// this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+  selectedItem: string | null = null;
+
+  selectItem(item: string) {
+    this.selectedItem = item;
+  }
+}
